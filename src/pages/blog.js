5459499396aac6d38bcb95ab1064de3bc7cf55a8
ChrisGraham427 <71577351+ChrisGraham427@users.noreply.github.com>
2021-05-22 22:07:25 -0700

    added more css

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -11,10 +11,13 @@ const BlogPage = ({ data }) => (
       <h1>Developer Discussion</h1>
       <p className="blog__description">
         A collection of my thoughts as I learn about development, I'll write
-        about the ups, the downs, the fails and the wins. It's all about the
-        journey with this one, so follow along and feel free to comment I would
-        love to hear everyone else's thoughts.
+        about the ups, the downs, the fails and the wins.
       </p>
+      <p className="blog__description">
+        From career transition to learning my first HTML tag syntax, this
+        discussion will explore my journey into the vast world of development.
+      </p>
+      <p className="blog__description"></p>
       {data.allMarkdownRemark.edges.map(post => (
         <div className="blog__post-box" key={post.node.id}>
           <h3 className="blog__post-title">{post.node.frontmatter.title}</h3>
