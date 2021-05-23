17134a57f9c2d128691d93b11bc182cc58eb0ea3
ChrisGraham427 <71577351+ChrisGraham427@users.noreply.github.com>
2021-05-22 20:56:00 -0700

    added more css

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -11,6 +11,7 @@ const ResumePage = props => (
     alt="background-hero"
     fluid={props.data.indexImage.childImageSharp.fluid}
     className="background__hero"
+    className="resume__main"
   >
     <Layout>
       <Seo title="Resume" />
@@ -21,7 +22,7 @@ const ResumePage = props => (
           formats={["AUTO", "WEBP", "AVIF"]}
           alt="Resume"
           className="resume__image"
-          style={{ maxWidth: 1500, maxHeight: 1800 }}
+          style={{ maxWidth: 1800, maxHeight: 1800 }}
         />
         <Link to="/">
           <button className="resume__button">HOME</button>
@@ -35,7 +36,7 @@ export default ResumePage
 
 export const pageQuery = graphql`
   query {
-    indexImage: file(relativePath: { eq: "background.jpg" }) {
+    indexImage: file(relativePath: { eq: "background2.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1800, quality: 100) {
           ...GatsbyImageSharpFluid
