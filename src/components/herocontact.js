0c9aacaeec2e1c625ca71bb15641cd588228a510
ChrisGraham427 <71577351+ChrisGraham427@users.noreply.github.com>
2021-06-04 11:10:17 -0700

    changed bottom nav

--- src/components/herocontact.js
+++ src/components/herocontact.js
@@ -12,34 +12,37 @@ const HeroContact = () => {
       <div className="herocontact">
         <div className="herocontact__section">
           <h3 className="herocontact__title">Home</h3>
-          <Link to="/">
-            <AiOutlineHome className="herocontact__icon" />
+          <Link className="herocontact__link" to="/">
+            <AiOutlineHome className="herocontact__icon--home" />
           </Link>
           <p className="herocontact__link-title">chrisgraham.dev</p>
         </div>
         <div className="herocontact__section">
           <h3 className="herocontact__title">GitHub</h3>
-          <Link to="https://github.com/ChrisGraham427">
-            <AiOutlineGithub className="herocontact__icon" />
+          <Link
+            className="herocontact__link"
+            to="https://github.com/ChrisGraham427"
+          >
+            <AiOutlineGithub className="herocontact__icon--github" />
           </Link>
           <p className="herocontact__link-title">ChrisGraham427</p>
         </div>
         <div className="herocontact__section">
           <h3 className="herocontact__title">LinkedIn</h3>
-          <Link to="https://www.linkedin.com/in/chriswgraham">
-            <AiOutlineLinkedin className="herocontact__icon" />
+          <Link
+            className="herocontact__link"
+            to="https://www.linkedin.com/in/chriswgraham"
+          >
+            <AiOutlineLinkedin className="herocontact__icon--linkedin" />
           </Link>
           <p className="herocontact__link-title">Chris W Graham</p>
         </div>
         <div className="herocontact__section">
           <h3 className="herocontact__title">Email</h3>
-          <AiOutlineMail className="herocontact__icon" />
-          <a
-            className="herocontact__link-title"
-            href="mailto:cwg.427@gmail.com"
-          >
-            cwg.427@gmail.com
-          </a>
+          <Link className="herocontact__link" href="mailto:cwg.427gmail.com">
+            <AiOutlineMail className="herocontact__icon--email" />
+          </Link>
+          <p className="herocontact__link-title">cwg.427@gmail.com</p>
         </div>
       </div>
       <hr className="herocontact__line" />
